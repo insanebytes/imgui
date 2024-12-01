60f65262db0ee8ccb0e4bcbdfe8f9a30e27713c2
insanebytes <andres.rs.gm@gmail.com>
2024-12-01 03:06:33 +0100

    arreglo proyecto premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -8,16 +8,8 @@ project "imgui"
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		"*.h",
+		"*.cpp"
 	}
 
 	filter "system:windows"
